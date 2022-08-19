940abdac4f15a915a82b8199391dcaa100e4bc81
Egor Antropov <antropov-zhora@mail.ru>
2022-08-19 15:37:45 +0300

    aaaaa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Задача 19");
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+
+if (number.Length == 5)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"Ваше число: {number} - палиндром.");
+    }
+    else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+else
+{
+    Console.WriteLine($"Введи правильное число");
+}
+
+
+
+
